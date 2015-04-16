3002049beeec789219ecf157070c75edd1672f3a
suleman <suleman@inonso.com>
2015-04-16 21:51:14 +0530

    sql file added

---
+++ croatia.sql
@@ -0,0 +1,122 @@
+/*
+SQLyog Community v11.51 (32 bit)
+MySQL - 5.5.41-0ubuntu0.12.04.1 : Database - croatia
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`croatia` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `croatia`;
+
+/*Table structure for table `accomodations` */
+
+DROP TABLE IF EXISTS `accomodations`;
+
+CREATE TABLE `accomodations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(125) NOT NULL COMMENT 'whether it is flat, room, duplex or villa',
+  `description` text,
+  `capacity` int(3) NOT NULL COMMENT 'no. of persons',
+  `price_per_person` decimal(11,0) NOT NULL,
+  `location__id` int(11) NOT NULL,
+  `images` varchar(125) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `accomodations` */
+
+/*Table structure for table `blogs` */
+
+DROP TABLE IF EXISTS `blogs`;
+
+CREATE TABLE `blogs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `body` text NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL,
+  `user__id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `blogs` */
+
+/*Table structure for table `comments` */
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment_body` text NOT NULL,
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `blog__id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `comments` */
+
+/*Table structure for table `festivals` */
+
+DROP TABLE IF EXISTS `festivals`;
+
+CREATE TABLE `festivals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `festival_name` varchar(125) NOT NULL,
+  `description` text,
+  `date_started` datetime NOT NULL,
+  `date_ended` datetime NOT NULL,
+  `location__id` int(11) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `festivals` */
+
+/*Table structure for table `locations` */
+
+DROP TABLE IF EXISTS `locations`;
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(125) NOT NULL,
+  `map_longitude` decimal(32,0) NOT NULL,
+  `map_lattitude` decimal(32,0) NOT NULL,
+  `images` varchar(125) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `locations` */
+
+/*Table structure for table `pages` */
+
+DROP TABLE IF EXISTS `pages`;
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `body` text,
+  `meta_title` varchar(64) DEFAULT NULL,
+  `meta_description` text,
+  `meta_keywords` text,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `pages` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
